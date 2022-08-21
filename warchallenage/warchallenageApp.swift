67bd01ef006f544263598fd71e0e73e44795bed2
William Meredith <williamleemeredith@gmail.com>
2022-08-21 16:39:11 -0400

    Initial Commit

---
+++ warchallenage/warchallenageApp.swift
@@ -0,0 +1,17 @@
+//
+//  warchallenageApp.swift
+//  warchallenage
+//
+//  Created by William Meredith on 8/21/22.
+//
+
+import SwiftUI
+
+@main
+struct warchallenageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
